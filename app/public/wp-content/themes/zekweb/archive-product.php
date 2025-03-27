87d57d0c363bd8fbd0d28a34680acedb4d0dabcc
mooms.dev <mooms.dev@gmail.com>
2025-03-27 20:25:06 +0700

    added some files

---
+++ app/public/wp-content/themes/zekweb/archive-product.php
@@ -0,0 +1,133 @@
+<?php get_header(); ?>
+<main id="main">
+    <div class="container">
+        <h2 class="heading">chăm sóc vết thương</h2>
+        <div class="row product-lists">
+            <div class="col-md-12 col-xl-4 product-item">
+                <a href="#" class="product-image">
+                    <img src="<?php bloginfo('template_url' ); ?>/images/bg-product.png" alt="Product image">
+                    <button class="btn btn-primary product-addcart">
+                        <i class="fa-solid fa-cart-shopping product-addcart__icon"></i>
+                        Thêm vào giỏ
+                    </button>
+                    <div class="product-addcart__overlay"></div>
+                </a>
+                <div class="product-title">Gel mờ sẹo Fasthealz Scar Kid</div>
+                <div class="product-description">
+                    Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                </div>
+                <div class="product-bottom">
+                    <div class="product-price">165.000đ</div>
+                    <a href="#">
+                        <button class="btn btn-primary product-looknow">Xem ngay</button>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-12 col-xl-4 product-item">
+                <a href="#" class="product-image">
+                    <img src="<?php bloginfo('template_url' ); ?>/images/bg-product.png" alt="Product image">
+                    <button class="btn btn-primary product-addcart">
+                        <i class="fa-solid fa-cart-shopping product-addcart__icon"></i>
+                        Thêm vào giỏ
+                    </button>
+                    <div class="product-addcart__overlay"></div>
+                </a>
+                <div class="product-title">Gel mờ sẹo Fasthealz Scar Kid</div>
+                <div class="product-description">
+                    Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                </div>
+                <div class="product-bottom">
+                    <div class="product-price">165.000đ</div>
+                    <a href="#">
+                        <button class="btn btn-primary product-looknow">Xem ngay</button>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-12 col-xl-4 product-item">
+                <a href="#" class="product-image">
+                    <img src="<?php bloginfo('template_url' ); ?>/images/bg-product.png" alt="Product image">
+                    <button class="btn btn-primary product-addcart">
+                        <i class="fa-solid fa-cart-shopping product-addcart__icon"></i>
+                        Thêm vào giỏ
+                    </button>
+                    <div class="product-addcart__overlay"></div>
+                </a>
+                <div class="product-title">Gel mờ sẹo Fasthealz Scar Kid</div>
+                <div class="product-description">
+                    Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                </div>
+                <div class="product-bottom">
+                    <div class="product-price">165.000đ</div>
+                    <a href="#">
+                        <button class="btn btn-primary product-looknow">Xem ngay</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="heading">chăm sóc vết sẹo</div>
+        <div class="row product-lists">
+            <div class="col-md-12 col-xl-4 product-item">
+                <a href="#" class="product-image">
+                    <img src="<?php bloginfo('template_url' ); ?>/images/bg-product.png" alt="Product image">
+                    <button class="btn btn-primary product-addcart">
+                        <i class="fa-solid fa-cart-shopping product-addcart__icon"></i>
+                        Thêm vào giỏ
+                    </button>
+                    <div class="product-addcart__overlay"></div>
+                </a>
+                <div class="product-title">Gel mờ sẹo Fasthealz Scar Kid</div>
+                <div class="product-description">
+                    Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                </div>
+                <div class="product-bottom">
+                    <div class="product-price">165.000đ</div>
+                    <a href="#">
+                        <button class="btn btn-primary product-looknow">Xem ngay</button>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-12 col-xl-4 product-item">
+                <a href="#" class="product-image">
+                    <img src="<?php bloginfo('template_url' ); ?>/images/bg-product.png" alt="Product image">
+                    <button class="btn btn-primary product-addcart">
+                        <i class="fa-solid fa-cart-shopping product-addcart__icon"></i>
+                        Thêm vào giỏ
+                    </button>
+                    <div class="product-addcart__overlay"></div>
+                </a>
+                <div class="product-title">Gel mờ sẹo Fasthealz Scar Kid</div>
+                <div class="product-description">
+                    Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                </div>
+                <div class="product-bottom">
+                    <div class="product-price">165.000đ</div>
+                    <a href="#">
+                        <button class="btn btn-primary product-looknow">Xem ngay</button>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-12 col-xl-4 product-item">
+                <a href="#" class="product-image">
+                    <img src="<?php bloginfo('template_url' ); ?>/images/bg-product.png" alt="Product image">
+                    <button class="btn btn-primary product-addcart">
+                        <i class="fa-solid fa-cart-shopping product-addcart__icon"></i>
+                        Thêm vào giỏ
+                    </button>
+                    <div class="product-addcart__overlay"></div>
+                </a>
+                <div class="product-title">Gel mờ sẹo Fasthealz Scar Kid</div>
+                <div class="product-description">
+                    Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                </div>
+                <div class="product-bottom">
+                    <div class="product-price">165.000đ</div>
+                    <a href="#">
+                        <button class="btn btn-primary product-looknow">Xem ngay</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<?php get_footer(); ?>
+ 
