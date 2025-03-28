8d47357286bf7f5f67d23dd144110ef0feaf41a9
newit-duylcq <newit-duylcq@newit.co.jp>
2025-03-29 01:21:19 +0700

    code home page sections

--- app/public/wp-content/themes/zekweb/page-home.php
+++ app/public/wp-content/themes/zekweb/page-home.php
@@ -2,9 +2,229 @@
 /*
  Template Name: Trang chủ
  */
- ?>
+?>
 <?php get_header(); ?>
 <main id="main">
-    
+    <div class="review bg-color">
+        <div class="container">
+            <h3 class="title text-primary text-uppercase">Khách hàng đánh giá về Fasthealz</h3>
+            <div class="row align-items-end mb-3r">
+                <div class="col-md-12 col-xl-6">
+                    <img src="https://placehold.co/506x506" alt="Placeholder Image" class="image">
+                </div>
+                <div class="col-md-12 col-xl-6">
+                    <div class="row g-5">
+                        <div class="col-md-12 col-xl-6">
+                            <img src="https://placehold.co/238x239" alt="Placeholder Image" class="image">
+                        </div>
+                        <div class="col-md-12 col-xl-6">
+                            <img src="https://placehold.co/238x239" alt="Placeholder Image" class="image">
+                        </div>
+                        <div class="col-md-12 col-xl-6">
+                            <img src="https://placehold.co/238x239" alt="Placeholder Image" class="image">
+                        </div>
+                        <div class="col-md-12 col-xl-6">
+                            <img src="https://placehold.co/238x239" alt="Placeholder Image" class="image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-xl-4">
+                    <div class="profile">
+                        <img src="https://placehold.co/259x258" alt="Placeholder Image" class="image rounded-circle">
+                        <h3 class="title">Bé mochi</h3>
+                        <div class="description text-center multi-line-4">
+                            Mỗi lần dùng con thích lắm, mà mẹ cũng yên tâm. Em xịt chống hăm không màu, không mùi, không nhờn dính nên mẹ dễ dàng vệ sinh
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 col-xl-4">
+                    <div class="profile">
+                        <img src="https://placehold.co/259x258" alt="Placeholder Image" class="image rounded-circle">
+                        <h3 class="title">Bé mochi</h3>
+                        <div class="description text-center multi-line-4">
+                            Mỗi lần dùng con thích lắm, mà mẹ cũng yên tâm. Em xịt chống hăm không màu, không mùi, không nhờn dính nên mẹ dễ dàng vệ sinh
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 col-xl-4">
+                    <div class="profile">
+                        <img src="https://placehold.co/259x258" alt="Placeholder Image" class="image rounded-circle">
+                        <h3 class="title">Bé mochi</h3>
+                        <div class="description text-center multi-line-4">
+                            Mỗi lần dùng con thích lắm, mà mẹ cũng yên tâm. Em xịt chống hăm không màu, không mùi, không nhờn dính nên mẹ dễ dàng vệ sinh
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="slider mb-5">
+        <div class="container">
+            <h3 class="title text-primary text-uppercase">ứng dụng nổi bật của fasthealz</h3>
+            <div class="description text-center">
+                Trong y tế và đời sống
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-xl-4">
+                    <img src="https://placehold.co/320x239" alt="Placeholder Image" class="image mb-3">
+                    <h3 class="title multi-line-1">Tiêu đề Tiêu đề Tiêu đề Tiêu đề</h3>
+                    <div class="description multi-line-3">Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả</div>
+                </div>
+                <div class="col-md-12 col-xl-4">
+                    <img src="https://placehold.co/320x239" alt="Placeholder Image" class="image mb-3">
+                    <h3 class="title multi-line-1">Tiêu đề Tiêu đề Tiêu đề Tiêu đề</h3>
+                    <div class="description multi-line-3">Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả</div>
+                </div>
+                <div class="col-md-12 col-xl-4">
+                    <img src="https://placehold.co/320x239" alt="Placeholder Image" class="image mb-3">
+                    <h3 class="title multi-line-1">Tiêu đề Tiêu đề Tiêu đề Tiêu đề</h3>
+                    <div class="description multi-line-3">Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="slider mb-5">
+        <div class="container">
+            <h3 class="title text-primary text-uppercase">octenidine - chất sát khuẩn hàng đầu châu âu</h3>
+            <div class="description text-center">
+                Trong y tế và đời sống
+            </div>
+            <div class="row">
+                <div class="col-md-12 col-xl-4">
+                    <img src="https://placehold.co/320x239" alt="Placeholder Image" class="image mb-3">
+                    <h3 class="title multi-line-1">Tiêu đề Tiêu đề Tiêu đề Tiêu đề</h3>
+                    <div class="description multi-line-3">Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả</div>
+                </div>
+                <div class="col-md-12 col-xl-4">
+                    <img src="https://placehold.co/320x239" alt="Placeholder Image" class="image mb-3">
+                    <h3 class="title multi-line-1">Tiêu đề Tiêu đề Tiêu đề Tiêu đề</h3>
+                    <div class="description multi-line-3">Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả</div>
+                </div>
+                <div class="col-md-12 col-xl-4">
+                    <img src="https://placehold.co/320x239" alt="Placeholder Image" class="image mb-3">
+                    <h3 class="title multi-line-1">Tiêu đề Tiêu đề Tiêu đề Tiêu đề</h3>
+                    <div class="description multi-line-3">Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả Mô tả</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="slider mb-5">
+        <div class="container">
+            <h3 class="title text-primary text-uppercase">bằng chứng khoa học</h3>
+            <div class="row mb-5">
+                <div class="col-md-12 col-xl-3">
+                    <img src="https://placehold.co/203x284" alt="Placeholder Image" class="image mb-5">
+                    <div class="description text-center">Giấy tờ 1</div>
+                </div>
+                <div class="col-md-12 col-xl-3">
+                    <img src="https://placehold.co/203x284" alt="Placeholder Image" class="image mb-5">
+                    <div class="description text-center">Giấy tờ 1</div>
+                </div>
+                <div class="col-md-12 col-xl-3">
+                    <img src="https://placehold.co/203x284" alt="Placeholder Image" class="image mb-5">
+                    <div class="description text-center">Giấy tờ 1</div>
+                </div>
+                <div class="col-md-12 col-xl-3">
+                    <img src="https://placehold.co/203x284" alt="Placeholder Image" class="image mb-5">
+                    <div class="description text-center">Giấy tờ 1</div>
+                </div>
+            </div>
+            <div class="w-100 text-center">
+                <div class="btn-home">
+                    <a href="#">Xem chi tiết</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mb-5">
+        <h3 class="title text-primary text-uppercase">Điểm tin mới nhất</h3>
+        <div class="row justify-content-between">
+            <div class="col-md-12 col-xl-6 mb-md-5">
+                <a href="#" class="d-block">
+                    <img src="https://placehold.co/439x290" alt="Placeholder Image" class="image rounded-4">
+                </a>
+                <h3 class="title multi-line-1 text-start mx-0 w-100">
+                    <a href="#" class="d-block">Title Title Title Title Title Title Title</a>
+                </h3>
+                <div class="description multi-line-3 mb-0">
+                    Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                </div>
+            </div>
+            <div class="col-md-12 col-xl-6">
+                <div class="description mb-0 text-center border-bottom border-1">
+                    CHĂM SÓC VẾT THƯƠNG VÀ CHỮA LÀNH SẸO
+                </div>
+                <div class="home-blogs">
+                    <div class="row mb-2">
+                        <div class="col-md-12 col-xl-4">
+                            <a href="#" class="d-block">
+                                <img src="https://placehold.co/156x105" alt="Placeholder Image" class="image rounded-4">
+                            </a>
+                        </div>
+                        <div class="col-md-12 col-xl-8">
+                            <div class="description multi-line-3 mt-4">
+                                Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 col-xl-4">
+                            <a href="#" class="d-block">
+                                <img src="https://placehold.co/156x105" alt="Placeholder Image" class="image rounded-4">
+                            </a>
+                        </div>
+                        <div class="col-md-12 col-xl-8">
+                            <div class="description multi-line-3 mt-4">
+                                Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 col-xl-4">
+                            <a href="#" class="d-block">
+                                <img src="https://placehold.co/156x105" alt="Placeholder Image" class="image rounded-4">
+                            </a>
+                        </div>
+                        <div class="col-md-12 col-xl-8">
+                            <div class="description multi-line-3 mt-4">
+                                Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 col-xl-4">
+                            <a href="#" class="d-block">
+                                <img src="https://placehold.co/156x105" alt="Placeholder Image" class="image rounded-4">
+                            </a>
+                        </div>
+                        <div class="col-md-12 col-xl-8">
+                            <div class="description multi-line-3 mt-4">
+                                Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-12 col-xl-4">
+                            <a href="#" class="d-block">
+                                <img src="https://placehold.co/156x105" alt="Placeholder Image" class="image rounded-4">
+                            </a>
+                        </div>
+                        <div class="col-md-12 col-xl-8">
+                            <div class="description multi-line-3 mt-4">
+                                Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả Mô tả, Mô tả, Mô tả, Mô tả, Mô tả, Mô tả,
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="w-100 text-center">
+            <div class="btn-home">
+                <a href="#">Xem thêm</a>
+            </div>
+        </div>
+    </div>
 </main>
 <?php get_footer(); ?>
