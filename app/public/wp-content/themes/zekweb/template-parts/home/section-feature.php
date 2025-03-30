3c8b6a477f0affd38e47fbbd0bf71b0e1dd92bef
mooms.dev <mooms.dev@gmail.com>
2025-03-30 12:55:52 +0700

    added section 1

---
+++ app/public/wp-content/themes/zekweb/template-parts/home/section-feature.php
@@ -0,0 +1,49 @@
+<?php
+  $feature = get_field('section1', 'option');
+  // $title = $feature['title'];
+  // $icon = $feature['icon'];
+  // $description = $feature['description'];
+  // $button = $feature['button'];
+?>
+
+<section class="section-feature">
+  <div class="container">
+    <div class="row no-gap-row">
+      <?php 
+        foreach ($feature as $item) :
+          $title = $item['title'];
+          $icon = $item['icon'];
+          $description = $item['description'];
+          $button = $item['button'];
+      ?>
+        <div class="col-12 col-lg-4 item">
+          <div class="feature__inner">
+            <?php 
+              if ($title) :
+                echo '<h3 class="title">' . $title . '</h3>';
+              endif; 
+            ?>
+
+            <div class="feature__description">
+              <?php 
+              if ($icon) :
+                echo '<img src="' . $icon . '" alt="' . $title . '">';
+              endif;
+              
+              if ($description) :
+                echo '<div class="description">' . $description . '</div>';
+              endif;
+              ?>
+            </div>
+
+            <?php
+              if ($button) :
+                echo '<a href="' . $button[0]['link'] . '" class="button rdu-3 text-center">' . $button[0]['text'] . ' <i class="fa-solid fa-arrow-right"></i></a>';
+              endif;
+            ?>
+          </div>
+        </div>
+      <?php endforeach; ?>
+    </div>
+  </div>
+</section>
