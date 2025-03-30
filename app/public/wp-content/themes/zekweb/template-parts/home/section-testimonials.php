2b5a3b591cbba88527da098f6f380305cd82994f
mooms.dev <mooms.dev@gmail.com>
2025-03-31 06:40:22 +0700

    Added section 5 - testimonials

---
+++ app/public/wp-content/themes/zekweb/template-parts/home/section-testimonials.php
@@ -0,0 +1,74 @@
+<?php
+$testimonials = get_field('section5', 'option');
+$title = $testimonials[0]['title'];
+$gallery = $testimonials[0]['gallery'];
+$list = $testimonials[0]['list'];
+?>
+
+<section class="review bg-color">
+  <div class="container">
+    <?php
+    if ($title):
+      echo '<h2 class="title text-primary text-uppercase">' . esc_html($title) . '</h2>';
+    endif;
+    ?>
+
+    <?php
+    if ($gallery):
+    ?>
+      <div class="row align-items-end mb-3r">
+        <?php
+        foreach ($gallery as $index => $image) :
+          // Fisrt image
+          if ($index === 0):
+            echo '<div class="col-md-12 col-xl-6 mb-5">
+                    <figure>
+                      <img src="' . esc_url($image) . '" alt="" class="image">
+                    </figure>
+                  </div>';
+          endif;
+        endforeach;
+        ?>
+
+        <div class="col-md-12 col-xl-6">
+          <div class="row g-5">
+            <?php
+            foreach ($gallery as $index => $image) :
+              if ($index !== 0):
+                echo '<div class="col-md-12 col-xl-6">
+                        <figure>
+                          <img src="' . esc_url($image) . '" alt="" class="image">
+                        </figure>
+                      </div>';
+              endif;
+            endforeach;
+            ?>
+          </div>
+        </div>
+      </div>
+    <?php
+    endif;
+    ?>
+
+    <div class="swiper swiper-testimonials">
+			<div class="swiper-wrapper">
+				<?php foreach ($list as $item) : ?>
+				<div class="swiper-slide">
+          <div class="profile">
+            <figure>
+              <img src="<?php echo $item['img']; ?>" alt="Placeholder Image" class="image rounded-circle">
+            </figure>
+
+            <h4 class="title"><?= $item['name']; ?></h4>
+
+            <div class="description text-center multi-line-4">
+              <?= $item['description']; ?>
+            </div>
+          </div>
+				</div>
+				<?php endforeach; ?>
+			</div>
+      <div class="swiper-pagination"></div>
+		</div>
+  </div>
+</section>
