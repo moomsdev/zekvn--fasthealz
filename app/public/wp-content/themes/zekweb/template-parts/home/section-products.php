f6d20338a79819e5b8083a26546aeae980ddec27
mooms.dev <mooms.dev@gmail.com>
2025-03-31 05:51:19 +0700

    Added section products

---
+++ app/public/wp-content/themes/zekweb/template-parts/home/section-products.php
@@ -0,0 +1,81 @@
+<?php
+  $products = get_field('section3', 'option');
+?>
+
+<section class="section-products">
+  <div class="container">
+    <?php 
+    foreach ($products as $product) : 
+      $title = $product['title'];
+
+      $args = [
+                'post_type'      => 'product',
+                'posts_per_page' => -1,
+                'tax_query' =>  [[ 'taxonomy' => 'product_cat', 'field'    => 'term_id', 'terms'    => $product['categories'], ]],
+              ];
+
+      if ($title) :
+        echo '<h2 class="title text-primary text-uppercase">' . esc_html($title) . '</h2>';
+      endif;
+    ?>
+      <div class="row">
+        <?php
+        $loop = new WP_Query($args);
+        if ($loop->have_posts()) : while ($loop->have_posts()) : $loop->the_post();
+          global $product;
+        ?>
+          <div class="col-12 col-md-4 product-item">
+              <div class="product-image">
+                <figure>
+                  <a href="<?= $product->get_permalink(); ?>">
+                    <?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?>
+                  </a>
+                </figure>
+              </div>
+
+              <div class="info">
+                <h3>
+                  <a class="product-title multi-line-2 text-center" href="<?= $product->get_permalink(); ?>"><?= $product->get_name(); ?></a>
+                </h3>
+
+                <div class="product-price text-center">
+                  <span class="old"><?= number_format( $product->get_price(), 0, ',', '.' ) . 'đ'; ?></span>
+                </div>
+              </div>
+
+              <div class="action d-flex gap-2">
+                <!-- Add to cart -->
+                <?php $product_id = $product->get_id(); ?>
+                <a href="?add-to-cart=<?php echo $product_id; ?>" 
+                  data-quantity="1" 
+                  class="button add_to_cart_button ajax_add_to_cart custom-add-to-cart" 
+                  data-product_id="<?php echo $product_id; ?>" 
+                  data-product_sku="<?php echo $product->get_sku(); ?>" 
+                  aria-label="Thêm sản phẩm vào giỏ hàng" 
+                  rel="nofollow">
+                  <i class="fa fa-shopping-cart"></i> Thêm vào giỏ
+                </a>
+                <!-- Buy now -->
+                <?php
+                $checkout_url = wc_get_checkout_url();
+                $buy_now_url = add_query_arg(['add-to-cart' => $product_id, 'buy_now' => '1'], $checkout_url );
+                ?>
+                <a href="<?php echo esc_url($buy_now_url); ?>" class="button buy-now-button">
+                  <img src="<?php bloginfo('template_url' ); ?>/images/buy-now.png" alt="buy-now">
+                  Mua ngay
+                </a>
+              </div>
+          </div>
+        <?php
+          endwhile;
+          wp_reset_postdata();
+        else :
+            echo 'Không tìm thấy sản phẩm.';
+        endif;
+        ?>
+      </div>
+    <?php
+    endforeach; 
+    ?>
+  </div>
+</section>
